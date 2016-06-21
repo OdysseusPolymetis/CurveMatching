d32c4f9ec9c8f37157dbd2d7db65066d3dc7c7e0
odysseus <odysseus@odysseus-XPS-13-9343>
2016-06-21 09:53:48 +0200

    Changes in lists

--- CurveMatching/src/fr/curve/api/StatsPerBook.java
+++ CurveMatching/src/fr/curve/api/StatsPerBook.java
@@ -28,32 +28,34 @@ public class StatsPerBook {
 				if (oeuvreParPartie.length>1){
 					List<String>chapitreParPartie=new ArrayList<String>();
 					for (String partie:oeuvreParPartie){
-						partie=partie.replaceAll("[\\t\\n\\r]", " ");
-						System.out.println("Partie : "+partie);
+						partie=partie.replaceAll("[\\t\\n\\r]+", " ");
 						String []chapitres=partie.split("Chapitre [1-9]+");
 						for (String str:chapitres){
-							str.replaceAll("\\n+", " ");
 							chapitreParPartie.add(str);
 						}
-					}
-					String numChapEtTxt[]=new String[2];
-					for(int counterChapitre=1; counterChapitre<chapitreParPartie.size()+1;counterChapitre++){
-						numChapEtTxt[0]="Chapitre "+counterChapitre;
-						numChapEtTxt[1]=chapitreParPartie.get(counterChapitre-1);
-						chapitresParOeuvre.add(numChapEtTxt);
+						
+						for(int counterChapitre=1; counterChapitre<chapitreParPartie.size()+1;counterChapitre++){
+							String numChapEtTxt[]=new String[2];
+							numChapEtTxt[0]="Chapitre "+counterChapitre;
+							numChapEtTxt[1]=chapitreParPartie.get(counterChapitre-1);
+							chapitresParOeuvre.add(numChapEtTxt);
+						}
 					}
 				}
 				else{
 					String oeuvreParChapitres[]=wholeText.split("Chapitre [0-9]+");
-					String numChapEtTxt[]=new String[2];
-					for(int counterChapitre=1; counterChapitre<oeuvreParChapitres.length+1;counterChapitre++){
-						numChapEtTxt[0]="Chapitre "+counterChapitre;
-						numChapEtTxt[1]=oeuvreParChapitres[counterChapitre-1];
+					
+					for(int counterChapitre=0; counterChapitre<oeuvreParChapitres.length;counterChapitre++){
+						oeuvreParChapitres[counterChapitre]=oeuvreParChapitres[counterChapitre].replaceAll("[\\t\\n\\r]+", " ");
+						String numChapEtTxt[]=new String[2];
+						numChapEtTxt[0]="Chapitre "+(counterChapitre);
+						numChapEtTxt[1]=oeuvreParChapitres[counterChapitre];
 						chapitresParOeuvre.add(numChapEtTxt);
 					}
 				}
 				mapDesOeuvres.put(file.getName(), chapitresParOeuvre);
 			}
 		}
+		System.out.println(mapDesOeuvres.get("Emile Zola-Au Bonheur des Dames (1972).txt").get(14)[1]);
 	}
 }
